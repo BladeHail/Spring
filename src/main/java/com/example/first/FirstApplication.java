bbc4e9ee13919b2667aacbc7d09c0fbc08a868f7
a01022456532-web <a01022456532@gmail.com>
2025-11-24 16:01:21 +0900

    Added login feature

--- src/main/java/com/example/first/FirstApplication.java
+++ src/main/java/com/example/first/FirstApplication.java
@@ -1,4 +1,4 @@
-package com.project.first;
+package com.example.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
 }
