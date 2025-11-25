228f0d95acd1f2532657221dbb7df15886005352
FredrickJ <jym352857@gmail.com>
2025-11-25 14:45:20 +0900

    Fixing Player Entity
    Getter Setter

--- src/main/java/com/example/first/entity/PlayerEntity.java
+++ src/main/java/com/example/first/entity/PlayerEntity.java
@@ -1,8 +1,13 @@
 package com.example.first.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class PlayerEntity {
     @Id
