2e0b7ad6dd84a2a834063672b5de8d9bb7299fe9
FredrickJ <jym352857@gmail.com>
2025-12-01 16:39:09 +0900

    Update GoogleUserInfo.java

--- src/main/java/com/example/first/security/oauth2/GoogleUserInfo.java
+++ src/main/java/com/example/first/security/oauth2/GoogleUserInfo.java
@@ -1,7 +1,12 @@
 package com.example.first.security.oauth2;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
+@Getter
+@NoArgsConstructor
 public class GoogleUserInfo implements OAuth2UserInfo {
     private Map<String, Object> attributes;
 
